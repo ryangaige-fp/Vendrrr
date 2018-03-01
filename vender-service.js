32c76758a8bb4aef169508be9b65ddd27db2f0cc
Darryl Kilzer <darrylkilzer@gmail.com>
2018-03-01 15:00:42 -0700

    some more snacks

--- vender-service.js
+++ vender-service.js
@@ -3,32 +3,38 @@ function VenderService(){
   var money = 0
 
   //our purchaseable items, add some of your own!
+  //we could add an id property to these items to display location for purchasing(A1, D4, etc)
   var items = [
     {
       name: "Fruit Snack Gummies",
       description: "Fruit salad done right.",
       price: 1.00,
-      amount: 10
+      amount: 45
     },
     {
       name: "Plant Food",
       description: "Are you hungry? I could use a light snack.",
       price: 5.00,
-      amount: 10
+      amount: 20
     },
     {
       name: "Admiral-able Ack-Bar",
       description: "Snack like an Admiral.",
-      img: "",
       price: .75,
-      amount: 10
+      amount: 3
     },
     {
       name: "Paranormal Snacktivity",
       description: "The perfect midnight snack.",
       price: .25,
-      amount: 10
+      amount: 7
     },
+    {
+      name: "The Void",
+      description: "This snack eats you!",
+      price: 3.25,
+      amount: 1
+    }
   ]
 
   //public parts
